ce3668005e916d181928ae73376445a0de4391c4
Penjani Mkandawire <mkandawire15@gmail.com>
2021-06-29 22:05:47 +0200

    Improved layout, added timing.

--- ICPC-2018.cpp
+++ ICPC-2018.cpp
@@ -1,73 +1,82 @@
 #include <iostream>
+#include <chrono>
 
 #include "Problem_1.h"
 
-void Problem_1()
+using std::list;
+using std::make_pair;
+using std::cout;
+using std::pair;
+using std::vector;
+using std::string;
+using std::endl;
+
+void Problem_1(list<pair<int, int>>& CharactersAndFrequency, vector<int>& Binary)
 {
-	std::list<std::pair<int, int>> CharactersAndFrequency1;
-	std::list<std::pair<int, int>> CharactersAndFrequency2;
+	auto start = std::chrono::high_resolution_clock::now();
 
-	Huffman Test;
-	{
-		CharactersAndFrequency1.push_back(std::make_pair(32, 1));
-		CharactersAndFrequency1.push_back(std::make_pair(72, 1));
-		CharactersAndFrequency1.push_back(std::make_pair(46, 1));
-		CharactersAndFrequency1.push_back(std::make_pair(100, 1));
-		CharactersAndFrequency1.push_back(std::make_pair(87, 1));
-		CharactersAndFrequency1.push_back(std::make_pair(101, 1));
-		CharactersAndFrequency1.push_back(std::make_pair(108, 3));
-		CharactersAndFrequency1.push_back(std::make_pair(111, 2));
-		CharactersAndFrequency1.push_back(std::make_pair(114, 1));
+	Huffman Hf;
+	
+	Leaf* Tree = Hf.BuildTree(CharactersAndFrequency);
 
-		std::vector<int> Binary({ 1,1,0,0,0,1,1,1,1,0,1,0,0,0,
-			1,1,1,0,1,1,0,1,0,0,0,1,0,1,0,0,1,1,0,1,1,1,1 });
+	string Decoded = Hf.DecodeText(Tree, Binary);
 
-		Leaf* Tree = Test.BuildTree(CharactersAndFrequency1);
+	int Frequency = Tree->GetFreq();
 
-		std::string Decoded = Test.DecodeText(Tree, Binary);
-		int Frequency = Tree->GetFreq();
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = stop - start;
+	auto f_secs = std::chrono::duration_cast<std::chrono::duration<float>>(duration);
 
-		std::cout << "Scenario 1:" << std::endl;
-		std::cout << "Decoded text: " << Decoded << std::endl;
-		std::cout << "Frequency: " << Frequency << std::endl;
-	}
-
-	{
-		CharactersAndFrequency2.push_back(std::make_pair(32, 3));
-		CharactersAndFrequency2.push_back(std::make_pair(44, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(46, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(83, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(97, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(99, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(101, 2));
-		CharactersAndFrequency2.push_back(std::make_pair(104, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(105, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(107, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(110, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(111, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(114, 1));
-		CharactersAndFrequency2.push_back(std::make_pair(116, 4));
-		CharactersAndFrequency2.push_back(std::make_pair(121, 1));
-
-		std::vector<int> Binary({ 0,0,0,0,1,0,0,1,0,0,0,0,1,0,1,0,1,1,1,
-			0,1,0,1,1,0,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,0,0,0,1,1,1,1,1,0,
-			1,1,1,1,1,1,0,0,0,0,1,0,0,0,1,1,0,0,1,0,1,1,1,0,0,1,0,0,1,1 });
-
-		Leaf* Tree = Test.BuildTree(CharactersAndFrequency2);
-
-		std::string Decoded = Test.DecodeText(Tree, Binary);
-		int Frequency = Tree->GetFreq();
-
-		std::cout << std::endl << "Scenario 2:" << std::endl;
-		std::cout << "Decoded text: " << Decoded << std::endl;
-		std::cout << "Frequency: " << Frequency << std::endl;
-	}
+	cout << "Decoded text: " << Decoded << endl;
+	cout << "Frequency: " << Frequency << endl;
+	cout << "Duration: " << f_secs.count() << " s" << endl;
 }
 
 int main()
 {
 	// Problem 1
-	Problem_1();
+	list<pair<int, int>> C_F1;
+	list<pair<int, int>> C_F2;
+
+	C_F1.push_back(make_pair(32, 1));
+	C_F1.push_back(make_pair(72, 1));
+	C_F1.push_back(make_pair(46, 1));
+	C_F1.push_back(make_pair(100, 1));
+	C_F1.push_back(make_pair(87, 1));
+	C_F1.push_back(make_pair(101, 1));
+	C_F1.push_back(make_pair(108, 3));
+	C_F1.push_back(make_pair(111, 2));
+	C_F1.push_back(make_pair(114, 1));
+
+	vector<int> Binary1({ 1,1,0,0,0,1,1,1,1,0,1,0,0,0,1,1,1,0,
+							 1,1,0,1,0,0,0,1,0,1,0,0,1,1,0,1,1,1,1 });
+
+	cout << "Scenario 1:" << endl;
+	Problem_1(C_F1, Binary1);
+
+	C_F2.push_back(make_pair(32, 3));
+	C_F2.push_back(make_pair(44, 1));
+	C_F2.push_back(make_pair(46, 1));
+	C_F2.push_back(make_pair(83, 1));
+	C_F2.push_back(make_pair(97, 1));
+	C_F2.push_back(make_pair(99, 1));
+	C_F2.push_back(make_pair(101, 2));
+	C_F2.push_back(make_pair(104, 1));
+	C_F2.push_back(make_pair(105, 1));
+	C_F2.push_back(make_pair(107, 1));
+	C_F2.push_back(make_pair(110, 1));
+	C_F2.push_back(make_pair(111, 1));
+	C_F2.push_back(make_pair(114, 1));
+	C_F2.push_back(make_pair(116, 4));
+	C_F2.push_back(make_pair(121, 1));
+
+	vector<int> Binary2({ 0,0,0,0,1,0,0,1,0,0,0,0,1,0,1,0,1,1,1,
+			0,1,0,1,1,0,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,0,0,0,1,1,1,1,1,0,
+			1,1,1,1,1,1,0,0,0,0,1,0,0,0,1,1,0,0,1,0,1,1,1,0,0,1,0,0,1,1 });
+
+	cout << endl << "Scenario 2:" << endl;
+
+	Problem_1(C_F2, Binary2);
 
 	return 0;
 }
